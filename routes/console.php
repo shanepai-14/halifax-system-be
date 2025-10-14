33d558db2e59f37a3d3a279d80a547d0ceb1dfd5
Shane <96838519+shanepai-14@users.noreply.github.com>
2025-10-14 12:01:13 +0800

    fix scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\RebuildSalesSummaries;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('sales:rebuild-summaries', function () {
+    $this->call(RebuildSalesSummaries::class);
+});
